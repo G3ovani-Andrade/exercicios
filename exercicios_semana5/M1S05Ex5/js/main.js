7335dfdbafdc35b747d20a8cdd682c3fb424f571
Geovani andrade <geovane_floripa@gmail.com>
2023-03-03 15:53:18 -0300

    adicionado icone de favorito no card filme

--- exercicios_semana5/M1S05Ex5/js/main.js
+++ exercicios_semana5/M1S05Ex5/js/main.js
@@ -1,3 +1,4 @@
+'use estrict'
 let filmes = [];
 let titulo = document.getElementById("titulo");
 let duracao = document.getElementById("duracao");
@@ -12,47 +13,24 @@ class Filme {
         this.nota = nota;
     }
 }
+filmes = [
+    new Filme("Mib", 150, 10),
+    new Filme("Meu malvado Favorito", 150, 9),
+    new Filme("Chuck", 150, 8),
+    new Filme("monstros SA", 150, 7),
+]
 
-function cadastrarFilme() {
-    let filme = new Filme(titulo.value, duracao.value, nota.value);
-    filme.favorito = false;
-    filme.assistido = false;
-    imagem = "https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U";
-       if(validarCampos()){
-            if (buscarFilme(titulo.value)) {
-                alerta.innerHTML = "<p>Já existe um filme com esse título</p>"
-                alerta.style.display = "block";
-                alertaForm();
-            } else {
-                alerta.innerHTML = "<p>Filme adicionado com sucesso</p>"
-                alerta.className = "sucesso"
-                alerta.style.display = "block";
-                alertaForm();
-                titulo.value ="";
-                duracao.value = "";
-                nota.value = "";
-                filmes.push(filme);
-                listarFilmes(filmes);
-            }
-       }
-    
-}
-
-let botaoCadastrarFilme = document.getElementById("btnCadastrarFilme");
-botaoCadastrarFilme.addEventListener("click", function () {
-    cadastrarFilme();
-});
-
-function buscarFilme(nomeFilme) {
-    const filmeEncontrado = filmes.find((filme) => filme.titulo === nomeFilme);
-    return filmeEncontrado;
-}
-
-
-function listarFilmes(listaFilmes) {
+const  listarFilmes = (listaFilmes) =>{
     console.log(listaFilmes)
-    document.getElementById("listaFilmes").innerHTML = filmes.map((filme) =>
+    if(listaFilmes.length==0){
+        alerta.innerHTML = "<p>Não foi encontrado nenhum filmes com esse título</p>"
+        alertaForm();
+    }
+    document.getElementById("listaFilmes").innerHTML = listaFilmes.map((filme) =>
     `<div class="cardFilme">
+        <div class="favoritos">
+            <img src="src/imagens/noFavorito.png" alt="">
+        </div>
         <img src="https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U" alt="">
         <div class="informacoesCard">
             <div class="notaCard">
@@ -71,8 +49,41 @@ function listarFilmes(listaFilmes) {
     ).join("");
 }
 
+listarFilmes(filmes);
+document.getElementById("pesquisaFilmes").addEventListener("input",e=>listarFilmes(buscarFilme(filmes,e.target.value)));
 
-function validarCampos(){
+const cadastrarFilme = () =>{
+    let filme = new Filme(titulo.value, duracao.value, nota.value);
+    filme.favorito = false;
+    filme.assistido = false;
+    imagem = "https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U";
+       if(validarCampos()){
+            if ((buscarFilme(filmes,titulo.value)).length==1) {
+                alerta.innerHTML = "<p>Já existe um filme com esse título</p>"
+                alerta.style.display = "block";
+                alertaForm();
+            } else {
+                alerta.innerHTML = "<p>Filme adicionado com sucesso</p>"
+                alerta.className = "sucesso"
+                alerta.style.display = "block";
+                alertaForm();
+                titulo.value ="";
+                duracao.value = "";
+                nota.value = "";
+                filmes.push(filme);
+                listarFilmes(filmes);
+            }
+       }
+}
+
+let botaoCadastrarFilme = document.getElementById("btnCadastrarFilme");
+botaoCadastrarFilme.addEventListener("click",() =>cadastrarFilme());
+
+const  buscarFilme = (listaFilmes,nomeFilme)=>listaFilmes.filter(filme=>filme.titulo.toLowerCase().includes(nomeFilme.toLowerCase()));
+
+
+
+const  validarCampos = ()=>{
     if(titulo.value === ""){
         alerta.innerHTML = "<p>Preencha o campo Título</p>"
         alertaForm();
@@ -91,7 +102,7 @@ function validarCampos(){
     }
    return true
 }
-function alertaForm(){
+const alertaForm = () =>{
     alerta.style.display = "block";
     setTimeout(() => {
         alerta.style.display = "none";
